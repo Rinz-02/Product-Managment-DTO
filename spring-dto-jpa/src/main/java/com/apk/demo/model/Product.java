5dc3ca1e41f9fe656e1580740fdac1e6146bc46c
Rinz-02 <willhan11122@gmail.com>
2026-03-12 08:00:16 +0400

    Created Models

---
+++ spring-dto-jpa/src/main/java/com/apk/demo/model/Product.java
@@ -0,0 +1,32 @@
+package com.apk.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String description;
+	private String price;
+	private String quantity;
+	private String status;
+	
+	@ManyToOne
+	private Category category;
+	
+}
