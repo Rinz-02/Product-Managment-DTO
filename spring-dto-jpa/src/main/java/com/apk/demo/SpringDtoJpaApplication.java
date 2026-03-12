92c29659f9207c0171c8698015756d43d20b145e
Rinz-02 <willhan11122@gmail.com>
2026-03-12 07:59:38 +0400

    Initial Commit

---
+++ spring-dto-jpa/src/main/java/com/apk/demo/SpringDtoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.apk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDtoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDtoJpaApplication.class, args);
+	}
+
+}
---
+++ spring-dto-jpa/src/test/java/com/apk/demo/SpringDtoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
