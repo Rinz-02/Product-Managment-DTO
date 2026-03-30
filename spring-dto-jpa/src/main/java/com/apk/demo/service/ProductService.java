3e31e68ffd9e1a29af11ccc59eada3b1d8554936
Rinz-02 <willhan11122@gmail.com>
2026-03-30 06:42:08 +0400

    Service

--- spring-dto-jpa/src/main/java/com/apk/demo/service/ProductService.java
+++ spring-dto-jpa/src/main/java/com/apk/demo/service/ProductService.java
@@ -2,8 +2,11 @@ package com.apk.demo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.apk.demo.model.Product;
 
+@Service
 public interface ProductService 
 {
 	public List<Product> findAll();
