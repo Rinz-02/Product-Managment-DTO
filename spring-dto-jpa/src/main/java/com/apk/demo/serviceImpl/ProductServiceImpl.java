1de90caf7dbca5eb71c4e93d7471e2cf588435b8
Rinz-02 <willhan11122@gmail.com>
2026-04-21 11:05:25 +0400

    fixed data issue

--- spring-dto-jpa/src/main/java/com/apk/demo/serviceImpl/ProductServiceImpl.java
+++ spring-dto-jpa/src/main/java/com/apk/demo/serviceImpl/ProductServiceImpl.java
@@ -1,32 +1,51 @@
 package com.apk.demo.serviceImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.apk.demo.mapper.CategoryMapper;
+import com.apk.demo.mapper.ProductMapper;
+import com.apk.demo.model.Category;
 import com.apk.demo.model.Product;
+import com.apk.demo.repository.CategoryRepository;
 import com.apk.demo.repository.ProductRepository;
+import com.apk.demo.request.CreateRequestProduct;
+import com.apk.demo.request.UpdateRequestProduct;
+import com.apk.demo.response.ResponseProduct;
 import com.apk.demo.service.ProductService;
 
+@Service
 public class ProductServiceImpl implements ProductService
 {
 
 	@Autowired
 	private ProductRepository productRepo;
 	
+	@Autowired
+	private CategoryRepository categoryRepo;
+	
 	@Override
-	public List<Product> findAll() {
-		return productRepo.findAll();
+	public List<ResponseProduct> findAll() 
+	{
+		List<Product> products =  productRepo.findAll();
+		return products.stream()
+				.map((product) -> {return ProductMapper.toResponse(product);})
+				.collect(Collectors.toList());	 
 	}
 
 	@Override
-	public Product save(Product product) {
-		return productRepo.save(product);
+	public ResponseProduct save(CreateRequestProduct product) 
+	{
+		return ProductMapper.toResponse(productRepo.save(ProductMapper.toEntity(product)));
 	}
 
 	@Override
-	public Product findById(Long id) {
-		return productRepo.findById(id).get();
+	public ResponseProduct findById(Long id) 
+	{
+		return ProductMapper.toResponse(productRepo.findById(id).get());
 	}
 
 	@Override
@@ -42,6 +61,28 @@ public class ProductServiceImpl implements ProductService
 		{
 			return false;
 		}
+	}
+
+	@Override
+	public ResponseProduct update(UpdateRequestProduct request) 
+	{
+		Product existing = productRepo.findById(request.getId())
+				.orElseThrow(() -> new RuntimeException("Product Not Found"));
+		
+		if(request.getName() != null) existing.setName(request.getName());
+		if(request.getPrice() != null) existing.setPrice(request.getPrice());
+		if(request.getQuantity() != null) existing.setQuantity(request.getQuantity());
+		if(request.getStatus() != null) existing.setStatus(request.getStatus());
+		
+		if(request.getCategoryId() != null) 
+		{
+			Category category = categoryRepo.findById(request.getCategoryId())
+					.orElseThrow(() -> new RuntimeException("ProductNotFound"));
+			existing.setCategory(category);
+		}
+ 		 productRepo.save(existing);
+ 		 
+ 		 return ProductMapper.toResponse(existing);
 	}
 
 }
